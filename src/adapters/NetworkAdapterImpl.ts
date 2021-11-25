685d776bb599d480d7d9094c3f18307ff561fbfd
Ard-Jan van Etten <ardjanve@gmail.com>
2021-11-25 13:37:38 +0100

    vervang Axios lambda caller door buildUrl methode

--- src/adapters/NetworkAdapterImpl.ts
+++ src/adapters/NetworkAdapterImpl.ts
@@ -2,14 +2,14 @@ import axios from "axios";
 import NetworkAdapter from "./NetworkAdapter";
 
 class NetworkAdapterImpl implements NetworkAdapter {
-    private url: String = "http://localhost:8080";
-    private stripSlash = (path: String) => path.charAt(0) == "/" ? path.slice(1) : path;
-    private callAxios = (axios: (url: string, data?: any | undefined) => Promise<any>, path: String, body: any): Promise<any> => axios(`${this.url}/${this.stripSlash(path)}`, body);
+    private url: string = "http://localhost:8080";
+    private stripSlash = (path: string) => path.charAt(0) == "/" ? path.slice(1) : path;
+    private buildUrl = (path: string): string => `${this.url}/${this.stripSlash(path)}`;
 
-    public post   = (path: String, body?: any | undefined): Promise<any> => this.callAxios(axios.post   , path, body);
-    public get    = (path: String, body?: any | undefined): Promise<any> => this.callAxios(axios.get    , path, body);
-    public put    = (path: String, body?: any | undefined): Promise<any> => this.callAxios(axios.put    , path, body);
-    public delete = (path: String, body?: any | undefined): Promise<any> => this.callAxios(axios.delete , path, body);
+    public post   = (path: string, body?: any | undefined): Promise<any> => axios.post   (this.buildUrl(path), body);
+    public get    = (path: string, body?: any | undefined): Promise<any> => axios.get    (this.buildUrl(path), body);
+    public put    = (path: string, body?: any | undefined): Promise<any> => axios.put    (this.buildUrl(path), body);
+    public delete = (path: string, body?: any | undefined): Promise<any> => axios.delete (this.buildUrl(path), body);
 }
 
 export default NetworkAdapterImpl;
