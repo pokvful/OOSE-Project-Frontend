8076cd7a5db963142d905ac906794b7a5b1d88b2
BartBarends <bart.barends.bb@gmail.com>
2021-11-30 13:20:15 +0100

    Frontend

--- src/dto/InterventionDTO.ts
+++ src/dto/InterventionDTO.ts
@@ -1,14 +1,10 @@
 class InterventionDTO {
     id: number = 0;
-    latitude: number = 0;
-    longitude: number = 0;
     name: string = "";
-    radius: number = 0;
 
     constructor(InterventionDTO?: InterventionDTO) {
         Object.assign(this, InterventionDTO);
     }
-
 }
 
 export default InterventionDTO;
